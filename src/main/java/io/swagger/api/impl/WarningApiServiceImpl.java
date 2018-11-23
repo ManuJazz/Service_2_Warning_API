12489d532f0237cedaf5741698cf50df730994c7
ManuJazz <mjesusaz@alumnos.unex.es>
2018-11-23 16:59:12 +0000

    Commit

--- src/main/java/io/swagger/api/impl/WarningApiServiceImpl.java
+++ src/main/java/io/swagger/api/impl/WarningApiServiceImpl.java
@@ -6,8 +6,7 @@ import io.swagger.model.*;
 import io.swagger.model.WarningItem;
 
 
-import java.util.Map;
-import java.util.List;
+import java.util.*;
 import io.swagger.api.NotFoundException;
 
 import java.io.InputStream;
@@ -22,29 +21,43 @@ import javax.validation.constraints.*;
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.java.JavaJerseyServerCodegen", date = "2018-11-23T15:48:07.165Z[GMT]")
 
 public class WarningApiServiceImpl extends WarningApiService {
+    List<WarningItem> lw=new ArrayList<WarningItem>();
     
     @Override
     public Response addwarning(WarningItem body, SecurityContext securityContext) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+        WarningItem wi=new WarningItem();
+        wi.setIdWarning(body.getIdWarning());
+        wi.setDate(body.getDate());
+        wi.setLength(body.getLength());
+        lw.add(wi);
+        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "Warning Added!")).build();
     }
     
     @Override
     public Response deleteWarning(Long warningId, String apiKey, SecurityContext securityContext) throws NotFoundException {
         // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+        for(int i=0;i<lw.size();i++){
+            if(lw.get(i).getIdWarning()==warningId)
+            lw.remove(lw.get(i));
+        }
+        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "Warning Deleted!")).build();
     }
     
     @Override
     public Response getWarningById(Long warningId, SecurityContext securityContext) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+        WarningItem wi=new WarningItem();
+
+        for(int i=0;i<lw.size();i++){
+            if(lw.get(i).getIdWarning()==warningId){
+               wi=lw.get(i);
+            }
+        }
+        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, wi.toString())).build();
     }
     
     @Override
     public Response getallwarnings(SecurityContext securityContext) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, lw.toString())).build();
     }
     
 }
